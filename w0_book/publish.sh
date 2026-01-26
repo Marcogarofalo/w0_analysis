ad76291ae47c1dcec6f77a42a50986564a17b7ef
Marcogarofalo <marcogarofalo31@gmail.com>
2026-01-26 16:10:23 +0100

    quarto

--- w0_book/publish.sh
+++ w0_book/publish.sh
@@ -1,1 +1,3 @@
-quarto publish quarto-pub --no-render  
+#quarto publish quarto-pub --no-render  
+# in r run
+rsconnect::deployApp(appDir = '~/analysis/flow/w0_analysis/w0_book/_book',recordDir = '~/analysis/flow/w0_analysis/w0_book', account = 'mgarofal')
