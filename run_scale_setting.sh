0838b68c24a41dc216e4c8d381f1c0c0a06c67a9
Marcogarofalo <marcogarofalo31@gmail.com>
2026-03-31 18:01:27 +0200

    Refactor command execution in run_scale_setting.sh for improved error handling and clarity

--- run_scale_setting.sh
+++ run_scale_setting.sh
@@ -1,37 +1,40 @@
 #!/bin/sh
 
-res=()
+# Array to store executables and their arguments
+commands=(
+    "./set_w0_a jack ../../data/jackknife/ ../../data/fit_all_beta ../data_ens_B64"
+    "./set_w0_a jack ../../data/jackknife/ ../../data/fit_all_beta ../data_ens_C80"
+    "./set_w0_a jack ../../data/jackknife/ ../../data/fit_all_beta ../data_ens_D96"
+    "./set_w0_a jack ../../data/jackknife/ ../../data/fit_all_beta ../data_ens_E112"
+    "./fit_cont_w0_fpi jack ../../data/jackknife/ ../../data/fit_all_beta"
+)
 
-run(){
-    local -n tmp=$2
-    local exe=$1
-    $exe || tmp+=("$exe")
+# Array to store failed commands
+failed=()
+
+# Function to run a command and capture failures
+run_command() {
+    local cmd="$1"
+    echo "Running: $cmd"
+    eval "$cmd"
+    if [ $? -ne 0 ]; then
+        failed+=("$cmd")
+    fi
 }
 
+# Run commands sequentially
+for cmd in "${commands[@]}"; do
+    run_command "$cmd"
+done
 
-run "
-./set_w0_a  jack ../../data/jackknife/  ../../data/fit_all_beta  ../data_ens_B64
-" res
-run "
-./set_w0_a  jack ../../data/jackknife/  ../../data/fit_all_beta  ../data_ens_C80
-" res
-run "
-./set_w0_a  jack ../../data/jackknife/  ../../data/fit_all_beta  ../data_ens_D96
-" res
-run "
-./set_w0_a  jack ../../data/jackknife/  ../../data/fit_all_beta  ../data_ens_E112
-" res
-
-
-run "
-./fit_cont_w0_fpi   jack ../../data/jackknife/  ../../data/fit_all_beta
-" res
+# Print results
 echo "===================="
 echo "Results:"
-
-for i in "${res[@]}"; do
-    echo "Failed test: $i"
-done
-if [ ${#res[@]} -eq 0 ]; then
-    echo "all good"
+if [ ${#failed[@]} -eq 0 ]; then
+    echo "All commands ran successfully."
+else
+    echo "Failed commands:"
+    for cmd in "${failed[@]}"; do
+        echo "$cmd"
+    done
 fi
